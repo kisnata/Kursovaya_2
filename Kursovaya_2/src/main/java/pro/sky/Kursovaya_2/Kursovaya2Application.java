91dd176df5c0a49c0991b38089ccd194c1cf2ace
Ната <nata.van4inova@yandex.ru>
2023-07-04 10:29:55 +0300

    Первый коммит

---
+++ Kursovaya_2/src/main/java/pro/sky/Kursovaya_2/Kursovaya2Application.java
@@ -0,0 +1,14 @@
+package pro.sky.Kursovaya_2;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kursovaya2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kursovaya2Application.class, args);
+	}
+
+}
---
+++ Kursovaya_2/src/test/java/pro/sky/Kursovaya_2/Kursovaya2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.Kursovaya_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kursovaya2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
